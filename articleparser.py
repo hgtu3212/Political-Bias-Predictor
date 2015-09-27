e577263b02d4247da715dae45415e381e5d9bc77
aakashshah <aakashshah@me.com>
2015-09-26 23:49:51 -0500

    finalized crawler/parser

--- articleparser.py
+++ articleparser.py
@@ -3,8 +3,7 @@ import urllib
 import lxml
 from bs4 import BeautifulSoup
 
-links = ["http://www.huffingtonpost.com/entry/john-boehner-departure-congress_5605b381e4b0768126fd952e",
-         "http://www.newyorker.com/news/news-desk/what-can-china-achieve-with-cap-and-trade"]
+links = ["http://www.nbcnews.com/news/religion/pope-francis-priests-can-forgive-abortion-if-women-are-contrite-n419321"]
 
 for link in links:
     # clean up html, getting rid of unwanted text
@@ -14,7 +13,7 @@ for link in links:
 
     # parse html
     article_soupify = BeautifulSoup(readable_article, "lxml")
-    print readable_title + article_soupify.get_text()
+    print readable_title + " " + article_soupify.get_text()
 
 
 
--- web_crawler.py
+++ web_crawler.py
@@ -1,23 +1,28 @@
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-from scrapy.item import Item, Field
+import newspaper
 
-class MyItem(Item):
-    url = Field()
+def get_links():
+    newspapers = [("cnn", "http://www.cnn.com/politics"), ("nytimes", "http://www.nytimes.com/politics"),
+              ("new yorker", "http://www.newyorker.com/news"), ("fox news", "http://www.foxnews.com/politics/index.html"),
+              ("breitbart", "http://www.breitbart.com/big-government/"), ("huffington post", "http://www.huffingtonpost.com/politics/"),
+              ("cnbc", "http://www.cnbc.com/politics/"), ("the blaze", "http://www.theblaze.com/stories/")]
 
-class MySpider(CrawlSpider):
-    name = 'huffpost.com'
-    allowed_domains = ['huffingtonpost.com']
-    start_urls = ['http://www.huffingtonpost.com/entry/andrew-cuomo-gun-control_5606e323e4b0dd850307cf4c?utm_hp_ref=politics']
-    print 'hi'
-    rules = (
-        # Extract links matching 'category.php' (but not matching 'subsection.php')
-        # and follow links from them (since no callback means follow=True by default).
-        Rule(LinkExtractor(allow=('huffingtonpost.com/'), deny=('live.huffingtonpost.com/r/highlight/' )), callback='parse_item', follow =True))
-    print 'hi2'
-    def parse_item(self, response):
-        print self, response
-        item = MyItem()
-        item['url'] = response.url
-        print "done"
-        return item
+    final_links = []
+    for tupl in range(len(newspapers)):
+        name = tupl[0]
+        news_link = tupl[1]
+
+        newspapr = newspaper.build(news_link)
+        for article in newspapr.article:
+            final_links.append((name, article.url))
+
+
+
+
+
+
+
+
+
+
+
+
