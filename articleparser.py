3898b376c5c0eb5b2376c7a09b16078bd818af7a
aakashshah <aakashshah@me.com>
2015-09-26 19:40:50 -0500

    html parser for links

---
+++ articleparser.py
@@ -0,0 +1,20 @@
+import readability
+import urllib
+import lxml
+from bs4 import BeautifulSoup
+
+links = ["http://www.huffingtonpost.com/entry/john-boehner-departure-congress_5605b381e4b0768126fd952e",
+         "http://www.newyorker.com/news/news-desk/what-can-china-achieve-with-cap-and-trade"]
+
+for link in links:
+    # clean up html, getting rid of unwanted text
+    html1 = urllib.urlopen(link).read()
+    readable_title = readability.Document(html1).short_title()
+    readable_article = readability.Document(html1).summary()
+
+    # parse html
+    article_soupify = BeautifulSoup(readable_article, "lxml")
+    print readable_title + article_soupify.get_text()
+
+
+
