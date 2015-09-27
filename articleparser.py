f35c10f0f8323e7934c155f283bee32052455c4a
rayraycano <rfc1@rice.edu>
2015-09-27 00:04:35 -0500

    fixed merge issues

--- articleparser.py
+++ articleparser.py
@@ -21,19 +21,6 @@ def parseArticles(listoflinks):
 	    article_soupify = BeautifulSoup(readable_article, "lxml")
 	    text.append(readable_title + article_soupify.get_text())
 	return text
-=======
-links = ["http://www.nbcnews.com/news/religion/pope-francis-priests-can-forgive-abortion-if-women-are-contrite-n419321"]
-
-for link in links:
-    # clean up html, getting rid of unwanted text
-    html1 = urllib.urlopen(link).read()
-    readable_title = readability.Document(html1).short_title()
-    readable_article = readability.Document(html1).summary()
-
-    # parse html
-    article_soupify = BeautifulSoup(readable_article, "lxml")
-    print readable_title + " " + article_soupify.get_text()
->>>>>>> 4b1d7cab9c1d573dfff74826ca9c93db6b7fdc1a
 
 
 
