84534726d3f853f31f25a55e3a5c882096646f44
rayraycano <rfc1@rice.edu>
2015-09-26 22:50:10 -0500

    updated project notes, test cases created, and string to vector is pretty much done with since the runtime is no good

--- String_to_Vector.py
+++ String_to_Vector.py
@@ -3,8 +3,9 @@
 #Raymond Cano
 import nltk
 from nltk.corpus import wordnet as wn
+import time 
 
-def create_bins(words):
+def create_bins(words, frequency_list):
 	"""
 	args: words is in (word.pos.number) format
 	"""
@@ -13,7 +14,7 @@ def create_bins(words):
 	array = words.copy()
 	for i in range(len(words)):
 		new_bin = set([])
-		main_word = words[i]
+		main_word = frequency_list.pop(0)
 		new_bin.add(main_word)
 		#we're done with word, don't iterate over it anymore
 		doneWith.add(words[i])
@@ -25,11 +26,27 @@ def create_bins(words):
 		bins.append(new_bin)
 	return bins
 
-def convertToWn(word):
-	return 
-
-
-
+def convertToWn(bagOfTokens):
+	"""
+	given a word, return the word's sense
+	"""
+	synsets = []
+	verbSet = set(['VBD', 'VB', 'VBN', 'VBG', 'VBZ', 'VBP'])
+	adjSet = set(['JJ', 'JJR', 'JJS'])
+	nounSet = set(['NN', 'NNS','NNP','NNPS'])
+	advSet = set(['RB', 'RBR', 'RBS'])
+	tagged = tag_unigrams(bagOfWords)
+	posTagged = []
+	for item in tagged:
+		if item[1] in adjSet:
+			posTagged.append(item[0] + '.a.01')
+		if item[1] in advSet:
+			posTagged.append(item[0] + '.r.01')
+		if item[1] in nounSet:
+			posTagged.append(item[0] + '.n.01')
+		if item[1] in verbSet:
+			posTagged.append(item[0] + '.v.01')
+	return posTagged
 
 
 def parse_sentence(sentence):
@@ -73,16 +90,27 @@ def find_similarity(dog, cat):
 	return wn.synset(dog).wup_similarity(wn.synset(cat))
 
 
+
+
 sentence = "Testing our basic parser"
-sentence2 = "Stores variables, mostly regular expressions, which may be language-dependent for correct application of the algorithm. An extension of this class may modify its properties to suit a language other than English; an instance can then be passed as an argument to PunktSentenceTokenizer and PunktTrainer constructors."
+sentence2 = " Stores variables, mostly regular expressions, which may be language-dependent for correct application of the algorithm. An extension of this class may modify its properties to suit a language other than English; an instance can then be passed as an argument to PunktSentenceTokenizer and PunktTrainer constructors."
 
 
 
+this = wn.synset('win.n.01')
 # print parse_sentence(sentence2)
 # print find_hyponyms('politician.n.01')
-for i in range(100):
-	find_similarity('republican.n.01', 'democrat.n.01')
+start = time.time()
+print this.hyponyms()
+end = time.time()
+print end - start 
 	# find_hyponyms('politician.n.01')
-# print tag_unigrams(parse_sentence(sentence))
+# teststring = ""
+# for i in range(100):
+# 	teststring += sentence2
+# items = tag_unigrams(parse_sentence(sentence2))
+# for i in range (len(items)):
+# 	print items[i][1]
+
 
 
